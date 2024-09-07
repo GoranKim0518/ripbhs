fdca923b978486d00aafb01fba8c248830d4efca
GoranKim0518 <109512293+GoranKim0518@users.noreply.github.com>
2024-09-07 15:49:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -103,43 +103,11 @@ function Play(x,y){
 //클릭시 랜덤 문구 경고창
 function mouseClicked(){
 		alert(indx[indxKeys[randKeys]]);
-	}
 
-// touchstart 이벤트 발생 여부
-var bStartEvent = false;
-// touchmove 이벤트 발생 여부
-var bMoveEvent = false;
-
-function init() {
-  document.addEventListener('touchstart', this.onStart.bind(this), false);
-  document.addEventListener('touchmove', this.onMove.bind(this), false);
-  document.addEventListener('touchend', this.onEnd.bind(this), false);
+function touchStarted(){
+	alert(indx[indxKeys[randKeys]]);
 }
-
-function onStart(e) {
-  bStartEvent = true;
-}
-
-function onMove(e) {
-  if (!bStartEvent) {
-    return;
-    // touchstart 이벤트가 발생하지 않으면 처리하지 않는다.
-  }
-  bMoveEvent = true;
-  // touchmove 이벤트 발생 여부를 설정한다.
-}
-
-function onEnd(e) {
-  if(bStartEvent && !bMoveEvent) {
-    // 클릭 이벤트로 판단한다.
-    alert(indx[indxKeys[randKeys]]);
-  }
-  // 각 플래그 값을 초기값으로 설정한다.
-  bStartEvent = false;
-  bMoveEvent = false;
-}
-
-
+	
 //반응형 캔버스
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
